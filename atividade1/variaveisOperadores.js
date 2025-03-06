dbca673555d55567fd349a1c6fe66b5ec0def4f0
Pedro Henrique S.C. Lima <117484349+PedroSCL@users.noreply.github.com>
2025-03-05 22:26:35 -0300

    Atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,25 @@
+let num1 = 10, num2 = 5;
+
+//adição
+let soma = num1 + num2;
+console.log("Resultado da soma entre " + num1 + " e " + num2 + " é " + soma);
+
+//subtração
+let subtracao = num1 - num2;
+console.log("Resultado da subtração entre " + num1 + " e " + num2 + " é " + subtracao);
+
+//multiplicação
+let multiplicacao = num1 * num2;
+console.log("Resultado da multiplicação entre " + num1 + " e " + num2 + " é " + multiplicacao);
+
+//divisão
+let divisao = num1 / num2;
+console.log("Resultado da divisão entre " + num1 + " e " + num2 + " é " + divisao);
+
+//resto da divisão
+let modulo = num1 % num2;
+console.log("Resultado do resto da divisão entre " + num1 + " e " + num2 + " é " + modulo);
+
+//exponenciação
+let exponenciacao = num1 ** num2;
+console.log("Resultado da exponenciação entre " + num1 + " e " + num2 + " é " + exponenciacao);
