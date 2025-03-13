97195d39dd458974716fc34e573ea8db958874ed
PedroSCL <pedrohenrique9353@gmail.com>
2025-03-13 08:55:42 -0300

    array_functions

---
+++ exemplosAulas/array_function.js
@@ -0,0 +1,15 @@
+let numA = 10;
+let numB = 5;
+let numC;
+
+numC = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${numC}`);
